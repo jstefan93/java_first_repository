296f4cf1965fa57cff237cf7092bc673f9817316
kubas <kubastefaniak@gmail.com>
2024-05-27 21:44:23 +0200

    Add some methods

--- src/MainApp.java
+++ src/MainApp.java
@@ -8,10 +8,23 @@ public class MainApp {
         stefan.firstName = "Stefan";
         stefan.lastName = "Kowalski";
         stefan.email = "stefan.kowalski@test.pl";
-        stefan.age = 30;
+        stefan.age = 12;
         stefan.isAdult = true;
-        System.out.println(stefan.firstName);
+        System.out.println(stefan.firstName + " " + stefan.lastName);
+        stefan.getFullName();
+        stefan.getAllInfo();
+        System.out.println(stefan.getUserAge());
+        int userAge = stefan.getUserAge();
+        System.out.println(userAge);
 
+        stefan.greetings("Judasz");
+        stefan.howOldAreYou("Judasz", 12);
+
+        int stefansAgePlus10 = stefan.yourAgePlus10(10);
+        System.out.println(stefansAgePlus10);
+
+        boolean userAdult = stefan.isUserAdult();
+        System.out.println("czy stefan jest dorosly? " + userAdult);
         User tomek = new User();
         System.out.println(tomek.email);
         tomek.firstName = "Tomek";
@@ -24,6 +37,9 @@ public class MainApp {
         System.out.println(tomek.email);
         System.out.println(tomek.age);
         System.out.println(tomek.isAdult);
+        tomek.getFullName();
+        tomek.getAllInfo();
+
     }
 
 }
--- src/User.java
+++ src/User.java
@@ -1,5 +1,5 @@
 public class User {
-
+    // z czego składa się klasa?
     // 1. Pola obiektu - opisują z czego składa się dany obiekt
 
     String firstName;
@@ -7,5 +7,49 @@ public class User {
     String email;
     int age;
     boolean isAdult;
+
+    // 2. Metody obiektu - opisują jak dany obiekt działa
+    // Metoda składa się ze zwracanego typu + nazwa metody + ciało metody w środku {}
+
+    void getFullName() {
+        System.out.println(firstName + " " + lastName);
+    }
+
+    void getAllInfo() {
+        System.out.println(firstName + " " + lastName + " " + email + " " + age + " " + isAdult);
+    }
+
+    int getUserAge() {
+        return age;
+    }
+
+    boolean isUserAdult() {
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    // PRZECIĄŻANIE METOD
+
+    void greetings(String name) {
+        System.out.println("Hi " + name + ". Nice to see you!");
+    }
+
+    void greetings(String name, String lastName) {
+        System.out.println("Hi " + name + " " + lastName + ". Nice to see you!");
+    }
+
+    void howOldAreYou(String name, int age) {
+        System.out.println("Hi " + name + ". You are " + age + " years old!");
+    }
+
+    int yourAgePlus10(int userAge) {
+        return userAge + 10;
+    }
+
+
 
 }
